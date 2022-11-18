8ce4a28ca3a3d3ded56bb8cfd0a85168c37013e5
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-18 10:03:24 +0300

    decrease train batch size

--- src/experiments/causal_modeling_experiments.py
+++ src/experiments/causal_modeling_experiments.py
@@ -33,7 +33,7 @@ def experiment_1():
     ).__dict__
 
     hyperparameters = PersonaChatHyperparametersV1(
-        train_batch_size=16,
+        train_batch_size=8,
         valid_batch_size=16,
         model_name="microsoft/DialoGPT-medium",
         predicted_texts_folder="/home/dimweb/Desktop/deeppavlov/persona_bot/predicted_texts",
