ebf673963ed4a75acbf2cbf1c7a66bfb50ab5bd2
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-19 01:46:54 +0300

    change model

--- src/train_scripts/causal_training.py
+++ src/train_scripts/causal_training.py
@@ -1,4 +1,5 @@
-from src.experiments.causal_modeling_experiments import experiment_1
+from src.experiments.causal_modeling_experiments import experiment_1, experiment_2
 
 if __name__ == "__main__":
-    experiment_1()
+    # experiment_1()
+    experiment_2()
