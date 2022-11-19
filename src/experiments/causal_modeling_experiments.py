828a588838b26ee08ca65ead13e95f44958a65ba
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-19 09:34:33 +0300

    change model

--- src/experiments/causal_modeling_experiments.py
+++ src/experiments/causal_modeling_experiments.py
@@ -25,6 +25,7 @@ def experiment_1():
     модели у которых сдвиг токенов происходит внутри модели
     - gpt2
     - microsoft/DialoGPT-medium
+    - roberta-base
     """
     parser = ExperimentArgumentParserV1()
     args: TrainArgumentsV1 = parser.args
@@ -42,7 +43,7 @@ def experiment_1():
     hyperparameters = PersonaChatHyperparametersV1(
         train_batch_size=8,
         valid_batch_size=16,
-        model_name="microsoft/DialoGPT-medium",
+        model_name="roberta-base",
         predicted_texts_folder="/home/dimweb/Desktop/deeppavlov/persona_bot/predicted_texts",
     )
 
