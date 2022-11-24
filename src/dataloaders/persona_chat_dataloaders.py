905b13db4784e4809a2f3f4b31665a808b1a3251
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-24 18:26:19 +0300

    create eda for seq2seq and causal

--- src/dataloaders/persona_chat_dataloaders.py
+++ src/dataloaders/persona_chat_dataloaders.py
@@ -17,7 +17,8 @@ class PersonaChatDatasetSampleV1(TypedDict):
 class PersonaChatDatasetV1(BaseInitialDatasetV1):
     """
     датасет общего назначения который предоставляет
-    интерфейс к оригинальному датасету никак не модифицируя его
+    интерфейс к оригинальному датасету никак не модифицируя его.
+    достаю из поля utterances диалоги и кладу рядом с ними персону
     """
 
     def _create_initial_dataset(
@@ -40,6 +41,42 @@ class PersonaChatDatasetV1(BaseInitialDatasetV1):
                         sample_id=sample_id,
                     )
                     dataset.append(dataset_item)
+
+        return dataset
+
+    def __getitem__(self, index: int) -> PersonaChatDatasetSampleV1:
+        return self.dataset[index]
+
+
+class PersonaChatDatasetV2(BaseInitialDatasetV1):
+    """
+    датасет общего назначения который предоставляет
+    интерфейс к оригинальному датасету никак не модифицируя его.
+    достаю из поля utterances самый последний диалог и
+    кладу рядом с ним персону.
+    данный датасет нужен для отладки длины данных в датасете
+    """
+
+    def _create_initial_dataset(
+        self,
+        initial_dataset: Dict,
+    ) -> List[PersonaChatDatasetSampleV1]:
+        dataset = []
+        for dialogue_id, dialogue in enumerate(initial_dataset):
+            persona = dialogue["personality"]
+            utterance = dialogue["utterances"][-1]
+            utterance_id = len(dialogue["utterances"]) - 1
+            history = utterance["history"]
+            sample_id = f"{dialogue_id}_{utterance_id}"
+            if len(history) % 2 == 1:
+                history.pop()
+            if len(history) > 0:
+                dataset_item = PersonaChatDatasetSampleV1(
+                    persona=persona,
+                    history=history,
+                    sample_id=sample_id,
+                )
+                dataset.append(dataset_item)
 
         return dataset
 
