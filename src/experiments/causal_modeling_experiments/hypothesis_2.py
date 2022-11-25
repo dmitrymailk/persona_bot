2c46d271e69ec2b80f26b3dc2d4d46dafe826e52
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-25 19:20:08 +0300

    change history length

--- src/experiments/causal_modeling_experiments/hypothesis_2.py
+++ src/experiments/causal_modeling_experiments/hypothesis_2.py
@@ -56,6 +56,7 @@ def h2_experiment_1():
         model_name="microsoft/DialoGPT-medium",
         predicted_texts_folder="./predicted_texts",
         debug_status=args.debug_status,
+        chat_history_pair_length=3,
     )
 
     tokenizer = AutoTokenizer.from_pretrained(hyperparameters.model_name)
