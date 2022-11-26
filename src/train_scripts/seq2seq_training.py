b8f485c5bae61ed027d10b1a5b723c101b760dfd
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-27 02:32:45 +0300

    add new hypothesis_3

--- src/train_scripts/seq2seq_training.py
+++ src/train_scripts/seq2seq_training.py
@@ -5,6 +5,11 @@ from src.experiments.seq2seq_modeling_experiments.seq2seq_hypothesis_2 import (
     h2_experiment_1,
 )
 
+from src.experiments.seq2seq_modeling_experiments.seq2seq_hypothesis_3 import (
+    h3_experiment_1,
+)
+
 if __name__ == "__main__":
     # h1_experiment_1()
-    h2_experiment_1()
+    # h2_experiment_1()
+    h3_experiment_1()
