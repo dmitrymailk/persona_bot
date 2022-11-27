d442fe7f0697622ea356b4db68551656c963f9e1
dmitrymailk <dmitrymail_k@mail.ru>
2022-11-27 15:13:28 +0300

    add hypothesis 3 for causal training

--- src/train_scripts/causal_training.py
+++ src/train_scripts/causal_training.py
@@ -7,7 +7,12 @@ from src.experiments.causal_modeling_experiments.causal_hypothesis_2 import (
     h2_experiment_1,
 )
 
+from src.experiments.causal_modeling_experiments.causal_hypothesis_3 import (
+    h3_experiment_1,
+)
+
 if __name__ == "__main__":
     # h1_experiment_1()
     # experiment_2()
-    h2_experiment_1()
+    # h2_experiment_1()
+    h3_experiment_1()
