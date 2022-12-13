b69df91fd5add44de475b065f96be82097b3f32e
dmitrymailk <dmitrymail_k@mail.ru>
2022-12-13 12:17:37 +0300

    change sampler

--- dimweb_persona_bot/experiments/causal_modeling_experiments/causal_hypothesis_1.py
+++ dimweb_persona_bot/experiments/causal_modeling_experiments/causal_hypothesis_1.py
@@ -88,7 +88,7 @@ def h1_experiment_1():
         tokenizer=tokenizer,
         base_train_dataset_class=PersonaChatDatasetV1,
         base_valid_dataset_class=PersonaChatDatasetV1,
-        base_train_sample_class=H1CausalTrainPersonaSampleV2,
+        base_train_sample_class=H1CausalTrainPersonaSampleV3,
         base_valid_sample_class=H1CausalValidPersonaSampleV1,
         debug_status=args.debug_status,
         device=device,
