1af85c65d8b6e99df08baff887110891434a2d68
dmitrymailk <dmitrymail_k@mail.ru>
2022-12-20 03:23:01 +0300

    delete useless optimizator

--- dimweb_persona_bot/lighting_models/causal_lighting_models.py
+++ dimweb_persona_bot/lighting_models/causal_lighting_models.py
@@ -8,7 +8,6 @@ from dimweb_persona_bot.utils import TextEvaluator
 from dimweb_persona_bot.database_logger.logger import DatabaseLoggerV1
 
 from pytorch_lightning import LightningModule
-from colossalai.nn.optimizer import HybridAdam
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers.optimization import get_linear_schedule_with_warmup
