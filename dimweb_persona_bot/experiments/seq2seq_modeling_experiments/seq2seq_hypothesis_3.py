bed875392493fefb1c805d7bf30b3c46d0c8b4bd
dmitrymailk <dmitrymail_k@mail.ru>
2022-12-20 21:13:48 +0300

    change model

--- dimweb_persona_bot/experiments/seq2seq_modeling_experiments/seq2seq_hypothesis_3.py
+++ dimweb_persona_bot/experiments/seq2seq_modeling_experiments/seq2seq_hypothesis_3.py
@@ -36,6 +36,7 @@ def h3_experiment_1():
     """
     - facebook/mbart-large-50
     - sberbank-ai/ruT5-base
+    - sberbank-ai/ruT5-large
     """
     parser = ExperimentArgumentParserV1()
     args: TrainArgumentsV1 = parser.args
@@ -45,7 +46,7 @@ def h3_experiment_1():
         max_epochs = 2
     devices = [args.cuda_device]
     lighting_hyperparameters = H1LightingHyperparametersV1(
-        precision=16,
+        precision=32,
         devices=devices,
         max_epochs=max_epochs,
     ).__dict__
@@ -53,7 +54,7 @@ def h3_experiment_1():
     hyperparameters = H2PersonaChatHyperparametersV1(
         train_batch_size=8,
         valid_batch_size=16,
-        model_name="facebook/mbart-large-50",
+        model_name="sberbank-ai/ruT5-large",
         predicted_texts_folder="./predicted_texts",
         debug_status=args.debug_status,
         model_architecture="seq2seq",
