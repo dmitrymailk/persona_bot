948808dd315b555afddb934c06cb7d88c13ed73f
dmitrymailk <dmitrymail_k@mail.ru>
2022-12-25 01:54:22 +0300

    padd labels

--- dimweb_persona_bot/experiments/seq2seq_modeling_experiments/seq2seq_hypothesis_5.py
+++ dimweb_persona_bot/experiments/seq2seq_modeling_experiments/seq2seq_hypothesis_5.py
@@ -1,2 +1,35 @@
+import logging
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional
+
+import datasets
+import numpy as np
+from datasets import load_dataset, load_from_disk
+
+import evaluate
+import transformers
+from transformers import (
+    AutoConfig,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    set_seed,
+)
+from transformers.trainer_utils import get_last_checkpoint
+from transformers.utils import check_min_version, send_example_telemetry
+from transformers.utils.versions import require_version
+
+
+# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
+check_min_version("4.25.1")
+
+
 def h5_experiment_1():
     print("h5_experiment_1")
+    # Set seed before initializing model.
+    set_seed(2022)
+    model_name = "facebook/mbart-large-50"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    dataset = load_from_disk("./datasets/ru_dialog_dataset_v1/")
